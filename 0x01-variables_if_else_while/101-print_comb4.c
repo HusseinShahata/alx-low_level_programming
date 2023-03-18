6f32803b46f565ee4d6a8addc819105dfec2e502
HusseinShahata <shahatahussein@gmail.com>
2023-03-18 12:53:02 -0700

    c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 
-/*
- *main - Write a program that prints all possible different combinations of three digits
- *Numbers must be separated by , followed by a space
- *The three digits must be different.
+/**
+ * main - Write a program that prints all possible
+ * idifferent combinations of three digits
+ * Numbers must be separated by , followed by a space
+ * The three digits must be different.
  *
  *
- * Returns: Always 0 success
+ * Return: Always 0.
+ *
  */
 
 int main(void)
 {
 	int i = '0';
-	int j = '0';
-        int k = '0';
-	
-	for(i = '0'; i <= '9'; i++)
+	int j, k;
+
+	for ((i = '0'); i <= '9'; i++)
 	{
-		for(j = i + 1; j <= '9'; j++)
+		for ((j = i + 1); j <= '9'; j++)
 		{
-			for(k = j + 1; k <= '9'; k++)
+			for ((k = j + 1); k <= '9'; k++)
 			{
 				putchar(i);
+
 				putchar(j);
+
 				putchar(k);
-					if(i == '7' && j == '8' && k == '9');
+
+				if ((i == '7') && (j == '8') && (k == '9'))
 					continue;
-				putchar(,);
+
+				putchar(',');
+
 				putchar(' ');
 			}
 		}
