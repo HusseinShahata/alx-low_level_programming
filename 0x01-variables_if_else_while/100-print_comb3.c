7a0263c4d2a992168493620955e38b59f507730d
HusseinShahata <shahatahussein@gmail.com>
2023-03-18 01:58:30 -0700

    c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*
+ * main - Write a program that prints all possible different combinations of two digits
+ * 	  Numbers must be separated by , followed by a space
+ * 	  The two digits must be different.
+ *
+ * Return : 0 success
+ */
+
+int main(void)
+{
+	int c = '01';
+
+	while (c <= 98)
+	{
+		putchar('c');
+		putchar(',');
+		putchar(' ');
+	}
+	c++;
+
+	putchar('\n');
+
+	return (0);
+}
