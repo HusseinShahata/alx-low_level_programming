e981b2253259916ebc77a1066ff300c9d5efb443
HusseinShahata <shahatahussein@gmail.com>
2023-03-18 15:08:03 -0700

    c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,51 +1,46 @@
-#!include <stdio.h>
+#include <stdio.h>
 
 /**
- * main - Write a program that prints all possible combinations of two two-digit numbers.
- * 	  The numbers should range from 0 to 99.
- * 	  The two numbers should be separated by a space.
- * 	  All numbers should be printed with two digits. 1 should be printed as 01.
- *
- *
+ * main - Write a program that prints all possible
+ * combinations of two two-digit numbers.
  *
  * Return: Always 0.
- *
  */
 
 int main(void)
 {
 	int i, j;
-        int k, l;
 
-        for ((i = '0'); i <= '9'; i++)
-        {
-                for ((j = i + 1); j <= '9'; j++)
-                {
-                        for ((k = j + 1); k <= '9'; k++)
-                        {
-				for((l = k + 1); l <= '9'; l++)
+	int k, l;
+
+	for ((i = '0'); i <= '9'; i++)
+	{
+		for ((j = i + 1); j <= '9'; j++)
+		{
+			for ((k = j + 1); k <= '9'; k++)
+			{
+				for ((l = k + 1); l <= '9'; l++)
 				{
-                                	putchar(i);
+					putchar(i);
 
-                                	putchar(j);
+					putchar(j);
 
 					putchar(' ');
-					
-                                	putchar(k);
+
+					putchar(k);
 
 					putchar(l);
-					
 
-	                                if ((i == '9') && (j == '8') && (k == '9') && (l == '9'))
-                                        	continue;
+					if ((i == '9') && (j == '8') && (k == '9') && (l == '9'))
+					continue;
 
-                                	putchar(',');
+					putchar(',');
 
-                                	putchar(' ');
+					putchar(' ');
 				}
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
-}	
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
