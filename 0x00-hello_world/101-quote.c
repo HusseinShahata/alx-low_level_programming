d70d7b877bbab2e8a5359a8437cf62cb3b033fcb
HusseinShahata <shahatahussein@gmail.com>
2023-03-19 09:47:12 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,8 @@
 #include <unistd.h>
 
 /**
- *main - Write a C program that prints exactly 
- and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line,
- *
- *
- * return: 1 if success.
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
 
 int main(void)
