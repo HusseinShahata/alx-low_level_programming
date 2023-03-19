b7a7150c385dddd48b00ded81d517ed0dfc2d3e8
HusseinShahata <shahatahussein@gmail.com>
2023-03-19 09:43:38 -0700

    c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Write a C program that prints exactly 
+ and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line,
+ *
+ *
+ * return: 1 if success.
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
