b474b7d4f6cb5f141d35884e985dda56024ff27d
HusseinShahata <shahatahussein@gmail.com>
2023-03-22 02:06:59 -0700

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int ch = 'a';
+
+	while(ch <= 'z')
+	{
+		_putchar('ch');
+		ch++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
