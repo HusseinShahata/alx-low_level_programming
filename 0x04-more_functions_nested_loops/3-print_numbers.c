b12c9c096a9188bfa95a34cd218c7a298193c1a3
HusseinShahata <shahatahussein@gmail.com>
2023-03-24 16:13:43 -0700

    c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,14 +7,12 @@
 *Return: returns nothing
 */
 
-void print_numbers(void);
+void print_numbers(void)
 {
-	int c;
-
-	for (c = 48; c <= 57; c++)
-	{	
-		_putchar(c);
-	}
-	
-	_putchar('\n');
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
 }
