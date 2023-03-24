5395834f64aab98bbca1565b052543a0d5182add
HusseinShahata <shahatahussein@gmail.com>
2023-03-24 13:47:11 -0700

    c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,15 +5,17 @@
  *
  * @c: character integer.
  * 
- * Returns: 1 if c is a digit 0 otherwise.
+ * Returns: 1 if 'c' is a digit 0 otherwise.
  *
  */
 
 int _isdigit(int c)
 {
 	if ((c >= 48) && (c <= 57))
-	return (1);
+		
+		return (1);
 
 	else
-	return (0);
+
+		return (0);
 }
