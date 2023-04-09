b06795445eb9cece18ff3253996835f359066fed
HusseinShahata <shahatahussein@gmail.com>
2023-04-09 11:52:36 -0700

    c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,14 +11,14 @@ char *string_toupper(char *)
 {
 	int i = 0;
 
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 
-		while ((s[i] >= 48 && s[i] <= 57) || (s[i] >= 65 && s[i] <= 90))
+		while ((s[i] >= 97 && s[i] <= 122 ) || (s[i] >= 65 && s[i] <= 90))
 		{
-			if (s[i] >= 48 && s[i] <= 57)
+			if (s[i] >= 97 && s[i] <= 122)
 			{
-				s[i] = s[i] + 17;
+				s[i] = s[i] + 32;
 			}
 		}
 		i++;
