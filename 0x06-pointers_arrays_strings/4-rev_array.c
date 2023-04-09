4446103b74488f5a2f106a564e606df65df76e20
HusseinShahata <shahatahussein@gmail.com>
2023-04-09 00:50:40 -0700

    c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses array of integers
+ * @a: input array
+ * @n: length of array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	while (a[n] <= '\0' && n != 0)
+		n--;
+}
+
