9e32887c6c3eec829facdca8b3df15079a81dddf
HusseinShahata <shahatahussein@gmail.com>
2023-04-10 03:31:51 -0700

    c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,13 +8,9 @@ char *cap_string(char *)
 	
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 97 && s[i] <= 122)
+		if (s[i] == sep[13] || (s[i] >= 97 && s[i] <= 122))
 		{
 			s[i] -= 32;
-		}
-		else if (!((s[i] == sep[13]) || (s[i] >= 65 && s[i] <= 90)))
-		{
-			break;
 		}
 	return (s);
 }
