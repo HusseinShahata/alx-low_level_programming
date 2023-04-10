04016e0e129a94161a91cf426dd1082fc66c8b03
HusseinShahata <shahatahussein@gmail.com>
2023-04-10 03:38:05 -0700

    c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+ * cap_string - a function that capitalizes
+ *              all words of a string
+ *
+ * @s: pointer to char input array
+ *
+ * Return: @s
+*/
 
 char *cap_string(char *)
 {
