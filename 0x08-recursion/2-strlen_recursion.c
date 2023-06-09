1e322660b3f01ab47d6a5c5ae95b17b371edca84
HusseinShahata <shahatahussein@gmail.com>
2023-06-08 18:44:04 -0700

    recursive

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,14 +6,14 @@
  * @s: pointer to a string.
  *
  * Return: length of a string.
- * 
+ *
  **/
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
-	return(_strlen_recursion(s + 1) + 1);
+	return (_strlen_recursion(s + 1) + 1);
 }
