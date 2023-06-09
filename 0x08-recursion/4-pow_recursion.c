6c0decfccf5d5b90e610c496b36e37c3a381fa9d
HusseinShahata <shahatahussein@gmail.com>
2023-06-08 19:53:50 -0700

    rec

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,10 +4,10 @@
  * _pow_recursion - Calculate x raised to the power of y.
  *
  * @x: given number.
- * 
+ *
  * @y: exponent.
  *
- * Return the value of x.
+ * Return: the value of x raised to y.
  **/
 
 int _pow_recursion(int x, int y)
