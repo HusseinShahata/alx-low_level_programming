19a3a9fad8407f47d88971a8926cb52665992027
HusseinShahata <shahatahussein@gmail.com>
2023-06-29 12:40:44 -0700

    c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,14 +20,10 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 
 	while (s1[i] != '\0')
-	{
 		i++;
-	}
 
 	while (s2[j] != '\0')
-	{
 		j++;
-	}
 
 	ar = malloc(sizeof(char) * (i + j + 1));
 
